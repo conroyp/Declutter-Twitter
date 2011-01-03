65d834d4d810f33819c699f4c8c495fb3d1948f6
Paul Conroy <paul@daft.ie>
2011-01-03 20:33:25 +0000

    First pass, remove tweets from display. Stream removal on reload not yet functional

--- chrome/content/decluttertwitter.js
+++ chrome/content/decluttertwitter.js
@@ -12,6 +12,7 @@
  */
 var declutterTwitter = function () {
     return {
+        filters: {},
         init : function () {
             gBrowser.addEventListener("load", function (event) {
                 var dcTab = window.top.getBrowser().selectedBrowser.contentWindow.location.href;
@@ -27,134 +28,98 @@ var declutterTwitter = function () {
                 if (onTwitter) {
 
                     // Just want to load jQuery the one time
-                    if(!twitterMediaPreview.jQuery)
+                    if(!declutterTwitter.jQuery)
                     {
                         var jsLoader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService(Components.interfaces.mozIJSSubScriptLoader);
 
                         // Load jquery in to the twitterMediaPreview namespace
-                        jsLoader.loadSubScript("chrome://twittermediapreview/content/jquery-1.4.4.min.js", twitterMediaPreview);
+                        jsLoader.loadSubScript("chrome://decluttertwitter/content/jquery-1.4.4.min.js", declutterTwitter);
 
                         // Set up noconflict jQuery so we don't kill other extensions
                         // tmp.jQuery is coming up as undefined after loader above, despite NS being set.
                         // Get around this by explicitly assigning it here.
-                        twitterMediaPreview.jQuery = jQuery.noConflict(true);
+                        declutterTwitter.jQuery = jQuery.noConflict(true);
                     }
 
-                    twitterMediaPreview.run();
                 }
             }, false);
         },
 
-        run : function () {
-            doc = document.defaultView;
+        removeTweets : function () {
+            // @TODO: Pull these from settings
+            this.filters = ['keywords', 'to', 'remove'];
 
-            // Imgur.com preview
-            window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.mediaType('twttr.media.types.Imgur',
-                {icon:'photo'
-                ,domain:'http://imgur.com'
-                ,matchers:{media:/imgur.com\/([a-zA-Z0-9\.]+)/g}
-                    ,process:function(A){
-                            // imgur.com (not sub-domain) doesn't have extension. Guess at jpeg.
-                        if(this.slug.indexOf('.')<0)
-                            this.slug = this.slug + '.jpg';
-                        this.data.src=this.slug;
-                        this.data.name=this.constructor._name;
-                        A()
-                    }
-                    ,render:function(B) {
-                        var img = 'http://i.imgur.com/{src}';
-                        var A='<div class=\'twitpic\'><a class=\'inline-media-image\' href=\''+img+'\' target=\'_blank\'> <img src=\''+img+'\' /></a></div>';
-                        twitterMediaPreview.jQuery(B, doc).append(window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.supplant(A,this.data))
-                    }
-                }
-            );
+            doc = window.content.document;
 
-
-            // Daft.ie SMI
-            // @TODO: Be smarter here and use this.jQuery rather than twittermediapreview.jquery
-            window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.mediaType('twttr.media.types.Daft',
-                {icon:'photo'
-                ,domain:'http://www.daft.ie/'
-                ,matchers:{media:/daft.ie\/([a-zA-Z0-9\.\/\?\=]+)/g}
-                ,process:function(A){
-                    this.data.src=this.slug;
-                    this.data.name=this.constructor._name;
-                    A()
-                }
-                ,render:function(B) {
-                    var price = '';
-                    var address = '';
-                    var photo = '';
-                    var details = '';
-                    var dt = this.data;
-
-                    var srcUrl = 'http://www.daft.ie/'+this.slug;
-                    twitterMediaPreview.jQuery.ajax({
-                        url: srcUrl,
-                        success: function(d) {
-                            var dommer = HTMLParser(d);
-                            address = twitterMediaPreview.jQuery(dommer).find('#smi_title h1').text();
-                            price = twitterMediaPreview.jQuery(dommer).find('#price').text();
-
-                            // Finance strap-line corrupts, strip it out..
-                            var finance = twitterMediaPreview.jQuery(dommer).find('#finance').text();
-                            price = price.replace(finance, '');
-
-                            details = twitterMediaPreview.jQuery(dommer).find('#smi_prop_type').text();
-                            photo = twitterMediaPreview.jQuery(dommer).find('.p1 img').attr('src');
-
-                           // If we haven't gotten an address, chances are this is a dead page/non-previewable
-                            if(address.length > 0)
-                            {
-                                var A='<div class=\'twitpic\'><strong><a class="inline-media-image" href="'+srcUrl+'" target="_blank">'+address+'</a><strong><br />'+details+', '+price+'<br /><a class="inline-media-image" href="'+srcUrl+'" target="_blank"><img src=\''+photo+'\' /></a></div>';
-                                twitterMediaPreview.jQuery(B, doc).append(window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.supplant(A,dt));
-                            }
+            var items = declutterTwitter.jQuery('.stream-item',doc);
+            declutterTwitter.jQuery.each(items, function(i, v)
+                {
+                    declutterTwitter.jQuery.each(declutterTwitter.filters, function(n, word)
+                    {
+                        if(declutterTwitter.jQuery(v, doc).html().toLowerCase().indexOf( word.toLowerCase() ) > -1)
+                        {
+                            declutterTwitter.jQuery(v, doc).remove();
                         }
                     });
+                });
+        },
+
+        run : function () {
+
+            this.removeTweets();
+
+            window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.streams.Stream.methods(
+            { _renderItemHtml: function (A)
+                {
+                    declutterTwitter.removeTweets();
+                    var items = declutterTwitter.jQuery('.stream-items');
+                    declutterTwitter.jQuery.each(items, function()
+                    {
+                        var c = declutterTwitter.jQuery(this).html();
+
+                        var p = declutterTwitter.jQuery(this);
+                        declutterTwitter.jQuery.each(declutterTwitter.filters, function(i, v)
+                        {
+                            if(c.toLowerCase().indexOf(v) > 0)
+                            {
+                                p.remove();
+                            }
+                        });
+                    });
+
+                    var fclean = true;
+                    declutterTwitter.jQuery.each(declutterTwitter.filters, function(i, v)
+                    {
+                        var chk = A.text + A.source;
+                        if(chk.toLowerCase().indexOf(v) > 0)
+                        {
+                            fclean = false;
+                        }
+                    });
+                    if(!fclean)
+                    {
+                        return '';
+                    }
+                    this.itemIds = this.itemIds || {};
+                    var B = this.streamItemId ? this.streamItemId(A) : A.id;this.itemIds[B] = 1;
+                    A.query = this.params.query;
+                    if (A.attributes)
+                    {
+                        A.attributes.query = this.params.query
+                    }
+
+                    // @TODO: Accessing constructor without subclassing (StreamTweet), not liked by firefox
+                    var si = window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr;
+                    var st = new si.views.StreamItem(A);
+
+                    return new twttr.views.StreamItem(
+                        {
+                            item_html: st.html(),item_id: B,item_type: this.streamItemType
+                        }
+                    ).html();
                 }
             });
-
-            // Adverts.ie SMI
-            // @TODO: Be smarter here and use this.jQuery rather than twittermediapreview.jquery
-            window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.mediaType('twttr.media.types.Adverts',
-                {icon:'photo'
-                ,domain:'http://www.adverts.ie/'
-                ,matchers:{media:/adverts.ie\/([a-zA-Z0-9\.\/\?\-\_\=]+)/g}
-                ,process:function(A){
-                    this.data.src=this.slug;
-                    this.data.name=this.constructor._name;
-                    A()
-                }
-                ,render:function(B) {
-                    var title = '';
-                    var price = '';
-                    var photo = '';
-
-                    var dt = this.data;
-
-                    var srcUrl = 'http://www.adverts.ie/'+this.slug;
-                    twitterMediaPreview.jQuery.ajax({
-                        url: srcUrl,
-                        success: function(d) {
-                            var dommer = HTMLParser(d);
-                            title = twitterMediaPreview.jQuery(dommer).find('.title h1').text();
-
-                            details = twitterMediaPreview.jQuery(dommer).find('.price dd').text();
-
-                            // Get rid of extra html string
-                            details = details.replace('Make an offer\/Ask a question', '');
-                            photo = twitterMediaPreview.jQuery(dommer).find('.p1 img').attr('src');
-                            // If we haven't gotten a title, chances are this is a dead page/non-previewable
-                            if(title.length > 0)
-                            {
-                                var A='<div class=\'twitpic\'><strong><a class="inline-media-image" href="'+srcUrl+'" target="_blank">'+title+'</a><strong><br />'+details+'<br /><a class="inline-media-image" href="'+srcUrl+'" target="_blank"><img src="'+photo+'" /></a></div>';
-                                twitterMediaPreview.jQuery(B, doc).append(window.top.getBrowser().selectedBrowser.contentWindow.wrappedJSObject.twttr.supplant(A,dt));
-                            }
-                        }
-                    });
-                }
-            }
-        );}
+        }
     };
 }();
 
